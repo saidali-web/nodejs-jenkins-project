8065be07de331939884718171a08aa30d40c96df
A SAIDALI <saidali0926@gmail.com>
2026-06-24 12:29:46 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOps Projectssssssssss 🚀");
+    res.send("this is my first pipeline 🚀");
 });
 
 app.get('/health', (req, res) => {
